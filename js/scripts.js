ee92543c0698ced6ddcff298d661505b51ff01fa
Dominic Montelongo <montelongodominic@email.com>
2019-06-14 11:01:47 -0700

    Add html and js; comparing previous work to current project objectives

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 //back end
+// Create a function "beepBoop"
+function beepBoop(userInput){
+
+}
+
+var number
+
+if (number )
 
 
 
 //front end
+$(document).ready(function(){
 
+})
 
 
 // extra
+// form#input
